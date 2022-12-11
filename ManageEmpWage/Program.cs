6f22e9fd1911c7d40953899995dd7127a9cbd23f
YogitaDevi <yogita.gry@gmail.com>
2022-12-11 14:02:05 +0530

    InterfaceApproach

---
+++ ManageEmpWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using ManageEmpWage;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+        empWageBuilder.computeEmpWage();
+
+    }
+}
